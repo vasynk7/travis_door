55e1df585307e5765d28efa058f4c408a43baa1f
vasynk7 <49471323+vasynk7@users.noreply.github.com>
2019-04-10 16:27:26 +0530

    Update test.c

--- test.c
+++ test.c
@@ -2,20 +2,14 @@
 #include <avr/io.h>
 #include<util/delay.h>
 #include<avr/interrupt.h>
-//volatile int Flag=0;
-//volatile int Flag1=0;
 volatile int pressure=0;
 volatile int belt=0;
 
 int main()
 {
- //   DDRD&=~(1<<2);
- //   DDRD&=~(1<<3);
-    DDRB&=~(1<<5);
+     DDRB&=~(1<<5);
     DDRC&=~(1<<3);
-    // for switch
-   // PORTD|=(1<<2);
-   // PORTD|=(1<<3);
+
     PORTB|=(1<<5); // pressure check
     PORTC|=(1<<3); // Seat belt check
     // led direction
@@ -28,7 +22,7 @@ int main()
     EIMSK|=(1<<INT0);
     EICRA|=(1<<ISC01);
     EIMSK|=(1<<INT1);
-   // PCIE0|=(1<<0);
+  
     PCICR|=(1<<0);
     PCMSK0|=(1<<5);
     PCICR|=(1<<1);
@@ -38,36 +32,19 @@ int main()
 
     while(1)
     {
-       /* if(Flag==1)
-        {
-            PORTB|=(1<<0);
-            _delay_ms(1000);
-            Flag=0;
-        }
-        else if(Flag1==1)
-        {
-            PORTB|=(1<<1);
-            _delay_ms(1000);
-            Flag1=0;
-        }
-        */
+      
         if(pressure==1)
         {
            if(belt==1)
         {
             PORTB|=(1<<0);
-            //PORTC|=(1<<2);
-           // _delay_ms(1000);
-            //Flag3=0;
-
+       
         }
         else
         {
          PORTB|=(1<<2);
          PORTB&=~(1<<0);
          PORTB&=~(1<<1);
-
-
         }
         }
         else
@@ -79,15 +56,7 @@ int main()
     }
     return 0;
 }
-  /*  ISR(INT0_vect)
-    {
-        Flag=1;
-    }
-    ISR(INT1_vect)
-    {
-        Flag1=1;
-    }
-    */
+
      ISR(PCINT0_vect)
     {
         pressure=1;
